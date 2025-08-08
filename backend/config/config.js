76f404c80227c506ee5f93b61f5f1f2019e6a839
Rafiputraaa <raafiputraa2@gmail.com>
2025-08-08 09:57:22 +0700

    deploy database

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres'
+//   }
+// };
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+    use_env_variable: 'DB_URI',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-};
+}
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,27 @@
+// const { Sequelize } = require('sequelize');
+
+// // Ganti konfigurasi ini sesuai dengan environment-mu
+// const db = new Sequelize('portfolio_web', 'postgres', 'aping123', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false, 
+// });
+
+// module.exports = db;
+
+// database.js
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// Ganti konfigurasi ini sesuai dengan environment-mu
-const db = new Sequelize('portfolio_web', 'postgres', 'aping123', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DB_URI, {
   dialect: 'postgres',
-  logging: false, 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
+  logging: false,
 });
 
 module.exports = db;
