ccd9408c9661934219935dcaea3fcb10a94dd4c6
Rafiputraaa <raafiputraa2@gmail.com>
2025-08-08 11:56:07 +0700

    fix: domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    images: {
-    domains: ['portfolio-prod.up.railway.app'], // tambahkan ini
-    },
     remotePatterns: [
+      // Untuk lokal
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000',
         pathname: '/uploads/**',
       },
+      // Untuk production (Railway)
+      {
+        protocol: 'https',
+        hostname: 'portfolio-prod.up.railway.app',
+        port: '',
+        pathname: '/uploads/**',
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
