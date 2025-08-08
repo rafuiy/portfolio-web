a2fd8f4c75a5ad10da6f59f1c30cbaf3e6a341a9
Rafiputraaa <raafiputraa2@gmail.com>
2025-08-08 10:49:24 +0700

    fix

--- backend/index.ts
+++ backend/index.ts
@@ -11,21 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://rafiportfolio.vercel.app', // domain frontend kamu
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
-// Tambahan ini supaya preflight request (OPTIONS) tidak diblok
-app.options('*', cors({
-  origin: 'https://rafiportfolio.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
